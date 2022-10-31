feab2c1fecdd04fd97827c5ed13d5510606dd85f
NG <nicholeguan2@gmail.com>
2022-10-31 15:27:45 -0700

    add inqurier prompt to prompt user when app starts

---
+++ server.js
@@ -0,0 +1,53 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        database: 'employees_db'
+    },
+    console.log(`Connected to the books_db database.`)
+);
+
+const question = [
+    {
+        type: 'list',
+        name: 'action',
+        message: "What would you like to do?",
+        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
+    },
+];
+
+function init() {
+    inquirer
+        .prompt(question)
+        .then((answer) => {
+            switch (answer.action) {
+                case 'view all departments':
+                    console.log("viewing departments");
+                    break;
+                case 'view all roles':
+                    console.log("viewing roles");
+                    break;
+                case "view all employees":
+                    console.log("viewing all employees");
+                    break;
+                case 'add a department':
+                    console.log("adding a department");
+                    break;
+                case 'add a role':
+                    console.log("addding a role");
+                    break;
+                case 'add an employee':
+                    console.log("adding an employee");
+                    break;
+                case 'update an employee role':
+                    console.log("updating an empoyee role");
+                    break;
+            }
+        });
+}
+
+init();
