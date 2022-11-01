56e1bd44044596e0a2165f7e2d4a2f1de8e4b9ae
NG <nicholeguan2@gmail.com>
2022-10-31 17:40:17 -0700

    remove continuous question prompt and create function to view all departments and prompt for question again

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const db = mysql.createConnection(
         password: '1234',
         database: 'employees_db'
     },
-    console.log(`Connected to the books_db database.`)
+    console.log(`Connected to the employees_db database.`)
 );
 
 const question = [
@@ -22,44 +22,52 @@ const question = [
 ];
 
 const init = async () => {
-    let selected = "";
-    while (selected != "Quit") {
-        await askQuestion()
-            .then((answer) => {
-                switch (answer.action) {
-                    case 'view all departments':
-                        console.log("viewing departments");
-                        break;
-                    case 'view all roles':
-                        console.log("viewing roles");
-                        break;
-                    case "view all employees":
-                        console.log("viewing all employees");
-                        break;
-                    case 'add a department':
-                        console.log("adding a department");
-                        break;
-                    case 'add a role':
-                        console.log("addding a role");
-                        break;
-                    case 'add an employee':
-                        console.log("adding an employee");
-                        break;
-                    case 'update an employee role':
-                        console.log("updating an empoyee role");
-                        break;
-                    case 'Quit':
-                        console.log("quitting");
-                        selected = "Quit";
-                        break;
-                }
-            });
-    }
+    askQuestion();
 }
 
 function askQuestion() {
-    return inquirer
-        .prompt(question);
+    let selected = "";
+    inquirer
+        .prompt(question)
+        .then((answer) => {
+            switch (answer.action) {
+                case 'view all departments':
+                    console.log("viewing departments");
+                    viewDepartments();
+                    break;
+                case 'view all roles':
+                    console.log("viewing roles");
+                    break;
+                case "view all employees":
+                    console.log("viewing all employees");
+                    break;
+                case 'add a department':
+                    console.log("adding a department");
+                    break;
+                case 'add a role':
+                    console.log("addding a role");
+                    break;
+                case 'add an employee':
+                    console.log("adding an employee");
+                    break;
+                case 'update an employee role':
+                    console.log("updating an empoyee role");
+                    break;
+                case 'Quit':
+                    console.log("quitting");
+                    selected = "Quit";
+                    break;
+            }
+        });
+}
+
+function viewDepartments() {
+    db.promise().query("SELECT * FROM department")
+        .then(([rows, fields]) => {
+            console.table(rows);
+            askQuestion();
+        })
+        .catch(console.log)
 }
 
 init();
